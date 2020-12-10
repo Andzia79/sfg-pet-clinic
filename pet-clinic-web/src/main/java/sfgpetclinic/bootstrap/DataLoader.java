e4d598f977a58a72a2d6ef176ec4a4f5433b0d36
Andzia79 <andzia.sliwa@wp.pl>
2020-12-10 13:52:32 +0100

    adding spring confing  closes #23

--- pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,9 +4,7 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.PetServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +13,11 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private  final VetService vetService;
+@Autowired
+    public DataLoader(OwnerService ownerService, VetService vetService) {
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
-
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
