9c4a30c0d62cc607abda687726fd6634d358fe5f
Andzia79 <andzia.sliwa@wp.pl>
2020-12-10 14:19:00 +0100

    tryin to list owners

--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerservice;
+
+    public OwnerController(OwnerService ownerservice) {
+        this.ownerservice = ownerservice;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerservice.findAll());
 
         return "owners/index";
     }
